9cebe104be199cb28376a382bf4d11efdc09dd98
wujunqiang836 <wujunqiang836@pingan.com.cn>
2018-05-03 10:35:17 +0800

    overlook

--- src/component/overlook/index.jsx
+++ src/component/overlook/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import data from './data.jsx'
 
 import styles from './index.less';
 
@@ -7,68 +8,76 @@ export default class Overlook extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        cNumber: 5,
+        cNumber: 13,
         rNumber: 8,
-        dataSource: []
+        dataSource: ''
       }
+      // this.renderBackground();
+    }
+
+    componentWillMount () {
+      this.setState({
+        cNumber: data.cNumber,
+        rNumber: data.rNumber,
+        dataSource: data.dataSource
+      })
     }
 
     componentWillReceiveProps (oldProps, newProps) {
-      if (oldProps.id === newProps.id) {
-        return false
-      } else {
+      // if (oldProps.id === newProps.id) {
+      //   return false
+      // } else {
         // axios.
-      }
+        this.setState({
+          cNumber: data.cNumber,
+          rNumber: data.rNumber,
+          dataSource: data.dataSource
+        })
+      // }
     }
 
     renderSpace () {
-      // const dataSource = this.state.dataSource
-      const { cNumber, rNumber } = this.state
+      const { dataSource } = this.state
+      const boxs = this.renderPlain();
+      dataSource.length && dataSource.map((item) => {
+        if (boxs[item.x]) boxs[item.x].props.children[item.y] = (<div key={item.x + '_' + item.y} className={styles.personal} />)
+      })
 
-      const boxs = [];
-      console.log('renderSpace')
-      for (let i; i <= cNumber; i++) {
-        for (let j; j <= rNumber; j++) {
-          if (i === 1 && j === 1) {
-            boxs.push(<span className={styles.plain_first_box} />)
-          } else if (i === 1) {
-            boxs.push(<span className={styles.plain_top_box} />)
-          } else if (j === 1) {
-            boxs.push(<span className={styles.plain_left_box} />)
-          } else {
-            boxs.push(<span className={styles.plain_box} />)
-          }
-          if (i === cNumber && j === rNumber) {
-            console.log(boxs)
-            return boxs;
-          }
-        }
-      }
+      return boxs;
     }
 
     renderPlain() {
-      const cNumber = 5;
-      const rNumber = 8;
+      const { cNumber, rNumber } = this.state
       const boxs = [];
-      console.log('renderPlain')
-      for (let i; i <= cNumber; i++) {
-        for (let j; j <= rNumber; j++) {
-          if (i === 1 && j === 1) {
-            boxs.push(<span className={styles.plain_first_box} />)
-          } else if (i === 1) {
-            boxs.push(<span className={styles.plain_top_box} />)
-          } else if (j === 1) {
-            boxs.push(<span className={styles.plain_left_box} />)
-          } else {
-            boxs.push(<span className={styles.plain_box} />)
-          }
+
+      for (let i = 0; i <= cNumber; i++) {
+        let rowBox = []
+        for (let j = 0; j <= rNumber; j++) {
+          rowBox.push(<div key={i + '_' + j} className={styles.plain_box} />)
           if (i === cNumber && j === rNumber) {
-            console.log(boxs)
             return boxs;
           }
         }
+        boxs.push(<div key={i}>{ rowBox }</div>)
       }
     }
+
+    // renderBackground () {
+    //   const cBackgroundNum = 20
+    //   const rBackgroundNum = 10
+    //   const backgroundBox = [];
+
+    //   for (let i = 0; i <= cBackgroundNum; i++) {
+    //     let rowBox = []
+    //     for (let j = 0; j <= rBackgroundNum; j++) {
+    //       rowBox.push(<div key={i + '_' + j} className={styles.plain_box} />)
+    //       if (i === cBackgroundNum && j === rBackgroundNum) {
+    //         return backgroundBox;
+    //       }
+    //     }
+    //     backgroundBox.push(<div key={i}>{ rowBox }</div>)
+    //   }
+    // }
 
     render() {
       return (
