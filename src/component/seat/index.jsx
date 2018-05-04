22e6785373f5c9581e442c9229912587ee0f808a
wujunqiang836 <wujunqiang836@pingan.com.cn>
2018-05-04 14:12:30 +0800

    @overlook

--- src/component/seat/index.jsx
+++ src/component/seat/index.jsx
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types'
 // import axios from 'axios';
+import { Tooltip } from 'components'
 
 import styles from './index.less';
 
-export default class Overlook extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        umId: props.umId
+export default class Seat extends Component {
+    // constructor(props) {
+    //   super(props)
+    //   this.state = {
+    //     umId: props.umId
+    //   }
+    // }
+
+    static propTypes = {
+      status: PropTypes.object,
+      handleModalStatus: PropTypes.func
+    }
+
+    handleClick = () => {
+      const { handleModalStatus, status } = this.props
+      console.log('status', status)
+      if (typeof handleModalStatus === 'function') {
+        handleModalStatus(status.umId, true)
       }
     }
 
-    componentWillReceiveProps (oldProps, newProps) {
-      // if (oldProps.id === newProps.id) {
-      //   return false
-      // } else {
-        // axios.
-        this.setState({
-          umId: newProps.umId
-        })
-      // }
-    }
-
     render() {
+      const { status } = this.props;
+
       return (
-        <div className={styles.box} >
-          {}
-        </div>
+        <Tooltip placement='top' title={status.umId} >
+          <div className={styles.box} onClick={this.handleClick} >
+            {status.sex + '-' + status.isNew + '-' + status.telephoneState + '-' + status.name}
+          </div>
+        </Tooltip>
         );
     }
 }
--- src/container/right/index.js
+++ src/container/right/index.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout, { Sider, Content } from 'components/layout';
 import TreeMenu from 'component/tree-menu';
 import RightHeader from 'component/right-header';
 import Overlook from 'component/overlook'
 
-export default (props) => (
-  <Layout>
-    <Sider span={19}>
-      <TreeMenu />
-    </Sider>
-    <Layout>
-      <Content>
-        <RightHeader />
-        <Overlook id={1} />
-      </Content>
-    </Layout>
-  </Layout>
-	);
+// export default (props) => (
+//   <Layout>
+//     <Sider span={19}>
+//       <TreeMenu />
+//     </Sider>
+//     <Layout>
+//       <Content>
+//         <RightHeader />
+//         {console.log('props', props)}
+//         <Overlook id={1} />
+//       </Content>
+//     </Layout>
+//   </Layout>
+// 	);
+
+export default class right extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: props.id
+    }
+  }
+
+  toggleTeam (id) {
+    this.setState({
+      id: id
+    })
+  }
+
+  render () {
+    const { id } = this.state
+    return (
+      <Layout>
+        <Sider span={19}>
+          <TreeMenu onToggle={this.toggleTeam} />
+        </Sider>
+        <Layout>
+          <Content>
+            <RightHeader id={id} />
+            <Overlook id={id} />
+          </Content>
+        </Layout>
+      </Layout>
+    )
+  }
+}
