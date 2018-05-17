32926b85ff971dffc422d6f4598803c45bf17cb0
lulu <lulu27753@163.com>
2018-05-17 14:00:26 +0800

    热加载恢复 && 路由配置publicPath

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import 'babel-polyfill';
 
 import Login from 'container/login';
@@ -10,7 +10,7 @@ import '../style/reset.less';
 
 
 const App = () => (
-  <Router basename='/spotManage'>
+  <Router>
     <div>
       <Switch>
         <Route exact path='/' component={Dashboard} />
