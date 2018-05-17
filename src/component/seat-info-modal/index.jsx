8538a93a4a459c485fa207133fb67080e650b75c
wujunqiang836 <wujunqiang836@pingan.com.cn>
2018-05-17 19:49:39 +0800

    @router

--- src/component/seat-info-modal/index.jsx
+++ src/component/seat-info-modal/index.jsx
@@ -70,7 +70,7 @@ export default class SeatInfoModal extends Component {
             <Indicator title='姓名' data={data.TMR_NAME} />
           </Col>
           <Col span={6} className={styles.col}>
-            <Indicator title='司龄' data={data.ONBOARD_AGE} />
+            <Indicator title='司龄' data={data.ONBOARD_AGE + '月'} />
           </Col>
           <Col span={6} className={styles.col}>
             <Indicator title='坐席业务模式' data={data.TMR_TYPE} />
@@ -85,7 +85,7 @@ export default class SeatInfoModal extends Component {
         </Row>
         <Row className={styles.row}>
           <Col span={8}>
-            <Indicator title='是否话务活跃' data={data.IS_ACTIVE} />
+            <Indicator title='是否话务活跃' data={data.IS_ACTIVE === 1 ? '是' : '否'} />
           </Col>
         </Row>
         <Divider className={styles.divider} />
@@ -94,13 +94,13 @@ export default class SeatInfoModal extends Component {
         </Row>
         <Row className={styles.row}>
           <Col span={8} className={styles.col}>
-            <Indicator title='当日累计车险保费' data={data.TODAY_TOTAL_CI_PREMIUM} />
+            <Indicator title='当日累计车险保费' data={data.TODAY_TOTAL_CI_PREMIUM + '元'} />
           </Col>
           <Col span={8} className={styles.col}>
             <Indicator title='当日累计核保通过客户数' data={data.SECURITY_THROUGH_CUST_CNT} width='70%' />
           </Col>
           <Col span={8}>
-            <Indicator title='当日累计非车保护费' data={data.TODAY_TOTAL_NCI_PREMIUM} />
+            <Indicator title='当日累计非车保护费' data={data.TODAY_TOTAL_NCI_PREMIUM + '元'} />
           </Col>
         </Row>
         <Divider className={styles.divider} />
@@ -120,19 +120,19 @@ export default class SeatInfoModal extends Component {
         </Row> */}
         <Row className={styles.row}>
           <Col span={8} className={styles.col}>
-            <Indicator title='实时有效通时(上一时段)' data={data.REALTIME_EFFEC_TALKTIME} width='75%' />
+            <Indicator title='实时有效通时(上一时段)' data={data.REALTIME_EFFEC_TALKTIME + 'h'} width='75%' />
           </Col>
           <Col span={8} className={styles.col}>
             <Indicator title='实时有效通次(上一时段)' data={data.REALTIME_EFFEC_TALKNUM} width='75%' />
           </Col>
           <Col span={8}>
-            <Indicator title='实时平均通时(上一时段)' data={data.REALTIME_AVG_TALKTIME} width='75%' />
+            <Indicator title='实时平均通时(上一时段)' data={data.REALTIME_AVG_TALKTIME + 'h'} width='75%' />
           </Col>
         </Row>
         <Row className={styles.row}>
           <Col span={8} className={styles.col}>
             <Indicator title='当日累计通时' data={data.TODAY_TOTAL_TALKTIME} />
-            <Tooltip placement='right' title={`昨日累计通时 ${data.LAST_SUM_TIME}`}>
+            <Tooltip placement='right' title={`昨日累计通时 ${data.LAST_SUM_TIME + 'h'}`}>
               <Icon type={data.TODAY_TOTAL_TALKTIME > data.LAST_SUM_TIME ? 'arrowup' : 'arrowdown'}
                 className={styles.fixIcon}
               />
@@ -146,7 +146,7 @@ export default class SeatInfoModal extends Component {
           </Col>
           <Col span={8}>
             <Indicator title='当日累计平均通时' data={data.TODAY_TOTAL_AVG_TALKTIME} />
-            <Tooltip placement='right' title={`昨日累计平均通时 ${data.LAST_SUM_AVG}`}>
+            <Tooltip placement='right' title={`昨日累计平均通时 ${data.LAST_SUM_AVG + 'h'}`}>
               <Icon type={data.TODAY_TOTAL_AVG_TALKTIME > data.LAST_SUM_AVG ? 'arrowup' : 'arrowdown'} className={styles.fixIcon} style={{ right: '8px' }} />
             </Tooltip>
           </Col>
--- src/container/dashboard/getMenuData.jsx
+++ src/container/dashboard/getMenuData.jsx
@@ -1,99 +1,25 @@
 const menuData = [{
   name: '人员管理',
   icon: 'user-group',
-  path: 'user',
-  // children: [{
-  //   name: '现场实时管理',
-  //   path: 'version-manage',
-  // }, {
-  //   name: '需求管理',
-  //   path: 'demand-manage',
-  // }, {
-  //   name: '场景管理',
-  //   path: 'scenario-manage',
-  // }, {
-  //   name: '用例管理',
-  //   path: 'user-case-manage',
-  // }, {
-  //   name: '业务树',
-  //   path: 'business-tree',
-  //   hideInMenu: true, // 不在菜单中显示
-  //   // target: '_blank', // 当path是http链接时，使用该参数
-  // }],
-}, {
-  name: '现场管理',
-  icon: 'appstore-o',
   path: 'dashboard',
-  // children: [{
-  //   name: '执行机管理',
-  //   path: 'server-list',
-  // }, {
-  //   name: '任务管理',
-  //   path: 'task-list',
-  // }],
-  }, {
-    name: '业务洞见',
-    icon: 'pro-sharealt',
-    path: 'business',
-    // children: [{
-    //   name: '执行机管理',
-    //   path: 'server-list',
-    // }, {
-    //   name: '任务管理',
-    //   path: 'task-list',
-    // }],
-  }, {
-  name: '智能报表',
-  icon: 'pro-table',
-  path: 'report',
-  // children: [{
-  //   name: '用户管理',
-  //   path: 'user-list',
-  // }, {
-  //   name: '字典管理',
-  //   path: 'basic-list',
-  // }],
-}, {
-  name: '信息安全',
-  icon: 'pro-safety',
-  path: 'infosafe',
-  // authority: 'guest',
-  // children: [{
-  //   name: '场景统计图',
-  //   path: 'scenario-chart',
-  // }, {
-  //   name: '用例统计图',
-  //   path: 'user-case-chart',
-  // }, {
-  //   name: '任务日志查询',
-  //   path: 'task-logo-search',
-  // }, {
-  //   name: '检查点日志',
-  //   path: 'check-log',
-  // }],
-}, {
-  name: '业绩看板',
-  icon: 'pro-chart',
-  path: 'achievement',
-  // authority: 'guest',
-  // children: [{
-  //   name: '场景统计图',
-  //   path: 'scenario-chart',
-  // }, {
-  //   name: '用例统计图',
-  //   path: 'user-case-chart',
-  // }, {
-  //   name: '任务日志查询',
-  //   path: 'task-logo-search',
-  // }, {
-  //   name: '检查点日志',
-  //   path: 'check-log',
-  // }],
-}, {
-  name: 'NotFound',
-  icon: 'warning-circle',
-  path: 'notfound',
-}
+  children: [{
+    name: '现场实时管理',
+    path: 'version-manage',
+  }],
+},
+// {
+//     name: '业务洞见',
+//     icon: 'pro-sharealt',
+//     path: 'business',
+//   }, {
+//   name: '智能报表',
+//   icon: 'pro-table',
+//   path: 'report',
+// }, {
+//   name: '智能预警',
+//   icon: 'pro-safety',
+//   path: 'warnning',
+// }
 ];
 function formatter(data, parentPath = '', parentAuthority) {
   return data.map((item) => {
