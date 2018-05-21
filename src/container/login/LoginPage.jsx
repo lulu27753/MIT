363d9e3714991fb42b2995f5284e38c207c2a75c
wujunqiang836 <wujunqiang836@pingan.com.cn>
2018-05-21 16:46:06 +0800

    加密

--- src/container/login/LoginPage.jsx
+++ src/container/login/LoginPage.jsx
@@ -107,19 +107,15 @@ return this.state.redirectTO ? <Redirect to={this.state.redirectTO} /> : (
 function doEncrypt(pwd) {
 	var keyX = '9D18DCF9A4C04E12FFFF68A5A43AE321D56E2E693B4DBAB6CBE263EA156B81B9';
 	var keyY = '68864B894038382F7649B088733CB9F4DE322953CC81C58FC76F28BC96D43AE7';
-	
-	console.log('CryptoJS', CryptoJS);
-	console.log('SM2CipherMode', window.SM2CipherMode);
-	console.log('SM2Cipher', SM2Cipher);
-
 	var msgData = CryptoJS.enc.Utf8.parse(pwd);
 	var cipherMode = SM2CipherMode.C1C3C2;
 	var cipher = new SM2Cipher(cipherMode);
+	var userKey;
 	if (keyX && keyY) {
-		var userKey = cipher.CreatePoint(keyX, keyY);
+		userKey = cipher.CreatePoint(keyX, keyY);
 	} else {
 		loadPublicKey();
-		var userKey = cipher.CreatePoint(keyX, keyY);
+		userKey = cipher.CreatePoint(keyX, keyY);
 	}
 	msgData = cipher.GetWords(msgData.toString());
 	var encryptData = cipher.Encrypt(userKey, msgData);
